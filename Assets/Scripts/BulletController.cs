d5887e6ed2993d73715f790d481e9958c3f00bb4
dStaicu <dangeorgestaicu@gmail.com>
2020-09-06 22:23:07 +0300

    destroy monster object when they collide with bullets

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if(collision.collider.name == "Monster(duck)")
+        {
+            Destroy(collision.gameObject);
+            Destroy(this.gameObject);
+        }
+            
+    }
+}

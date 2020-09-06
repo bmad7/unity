a3005edcd264cf73d08ab9bdeac9556eded236b8
dStaicu <dangeorgestaicu@gmail.com>
2020-09-06 14:48:39 +0300

    Shooting

---
+++ Assets/Scripts/ShootingController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingController : MonoBehaviour { 
+
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+    private float bulletSpeed = 200.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }
+    }
+
+    public void Shoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.AddForce(firePoint.transform.up * bulletSpeed);
+
+        Debug.Log(rb.position);
+    }
+}
